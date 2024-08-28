b2f16d88c08e05aaed968e004b7d80ce4e4d4a07
William Muli <willi.wambu@gmail.com>
2024-08-28 19:01:59 +0300

    Created Aof struct for managing append only file

---
+++ aof.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+type Aof struct {
+	file *os.File
+	rd   *bufio.Reader
+	mu   sync.Mutex
+}
+
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
+	}
+
+	// start a goroutine to sync AOF to disk every second
+	go func() {
+		for {
+			aof.mu.Lock()
+			err := aof.file.Sync()
+			if err != nil {
+				log.Println("Error syncing AOF file: ", err)
+			}
+			aof.mu.Unlock()
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return aof, nil
+}
+
+func (aof *Aof) Write(value Value) error {
+	aof.mu.Lock()
+
+	defer aof.mu.Unlock()
+
+	_, err := aof.file.Write(value.Marshal())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (aof *Aof) Close() error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	return aof.file.Close()
+}
+
+func (aof *Aof) Read(fn func(value Value)) error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	aof.file.Seek(0, io.SeekStart)
+
+	reader := NewResp(aof.file)
+
+	for {
+		value, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		fn(value)
+	}
+
+	return nil
+}
